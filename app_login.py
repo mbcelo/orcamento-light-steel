e86b2c47e785df0be440b9bb1962d05b892e297a
mbcelo <134852474+mbcelo@users.noreply.github.com>
2025-07-16 20:34:17 -0300

    app_login.py

--- app_login.py
+++ app_login.py
@@ -1,56 +1,30 @@
-import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
-import app_gerador_pdf
-
-st.set_page_config(page_title="🔐 Login Steel Facility", layout="centered")
-
-# Carrega config
-with open('config_login.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-# Cria autenticação
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-
-# Login
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status:
-    authenticator.logout("🚪 Logout", "sidebar")
-    st.sidebar.success(f"Logado como: {name}")
-    app_gerador_pdf.executar_app()
-
-elif authentication_status is False:
-    st.error("Usuário ou senha incorretos")
-
-elif authentication_status is None:
-    st.warning("Insira suas credenciais")
-
-# Registro de usuário (opcional)
 with st.expander("✍️ Registrar novo usuário"):
     new_username = st.text_input("Novo usuário")
     new_name = st.text_input("Nome completo")
     new_email = st.text_input("Email")
     new_password = st.text_input("Senha", type="password")
+    confirm_password = st.text_input("Confirmar senha", type="password")
 
     if st.button("Registrar"):
-        if all([new_username, new_name, new_email, new_password]):
-            new_hashed_pw = stauth.Hasher([new_password]).generate()[0]
+        if not all([new_username, new_name, new_email, new_password, confirm_password]):
+            st.warning("Preencha todos os campos!")
+        elif new_password != confirm_password:
+            st.error("As senhas não conferem")
+        elif new_username in config['credentials']['usernames']:
+            st.error("Este usuário já existe")
+        else:
+            # Gerar a senha criptografada
+            hashed_pw = stauth.Hasher([new_password]).generate()[0]
+
+            # Adicionar novo usuário
             config['credentials']['usernames'][new_username] = {
                 'name': new_name,
                 'email': new_email,
-                'password': new_hashed_pw
+                'password': hashed_pw
             }
 
+            # Salvar no config_login.yaml
             with open('config_login.yaml', 'w') as file:
                 yaml.dump(config, file, default_flow_style=False)
+
             st.success(f"Usuário '{new_username}' registrado com sucesso!")
-        else:
-            st.warning("Preencha todos os campos para registrar")
