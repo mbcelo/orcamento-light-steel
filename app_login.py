e6ee8434778bc1a4314376f283538ed2ca1948d3
mbcelo <134852474+mbcelo@users.noreply.github.com>
2025-07-16 20:44:26 -0300

    app_login.py

--- app_login.py
+++ app_login.py
@@ -1,3 +1,39 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import app_gerador_pdf
+
+st.set_page_config(page_title="🔐 Login Steel Facility", layout="centered")
+
+# Carrega config
+with open('config_login.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Autenticação
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status:
+    authenticator.logout("🚪 Logout", "sidebar")
+    st.sidebar.success(f"Logado como: {name}")
+    # Passa usuário atual para o app principal
+    app_gerador_pdf.executar_app(username)
+
+elif authentication_status is False:
+    st.error("Usuário ou senha incorretos")
+
+elif authentication_status is None:
+    st.warning("Insira suas credenciais")
+
+# Registro
 with st.expander("✍️ Registrar novo usuário"):
     new_username = st.text_input("Novo usuário")
     new_name = st.text_input("Nome completo")
@@ -13,17 +49,14 @@ with st.expander("✍️ Registrar novo usuário"):
         elif new_username in config['credentials']['usernames']:
             st.error("Este usuário já existe")
         else:
-            # Gerar a senha criptografada
             hashed_pw = stauth.Hasher([new_password]).generate()[0]
 
-            # Adicionar novo usuário
             config['credentials']['usernames'][new_username] = {
                 'name': new_name,
                 'email': new_email,
                 'password': hashed_pw
             }
 
-            # Salvar no config_login.yaml
             with open('config_login.yaml', 'w') as file:
                 yaml.dump(config, file, default_flow_style=False)
 
